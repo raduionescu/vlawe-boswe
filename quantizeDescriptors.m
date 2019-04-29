6d7b8a294eb96caef8427df70bf2800e7da0e43b
raduionescu <raducu.ionescu@gmail.com>
2019-04-29 15:40:07 +0300

    Delete quantizeDescriptors.m

--- quantizeDescriptors.m
+++
@@ -1,5 +0,0 @@
-function nn = quantizeDescriptors(vocabulary, descriptors)
-% Quantize a visual descriptor to get a visual word
-
-nn = vl_kdtreequery(vocabulary.kdtree, vocabulary.centers, descriptors, 'MAXNUMCOMPARISONS', 15);
-
